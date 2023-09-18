e66dde62b15c0557752bb74d297aa1504992f03b
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-09-18 03:15:08 -0300

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Cristóbal Contreras on 18-09-23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
