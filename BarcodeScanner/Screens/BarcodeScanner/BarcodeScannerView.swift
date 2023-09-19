19d2fc4ba53b5dd85154fae9681ecb5a7edffb49
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-09-19 00:51:21 -0300

    feat: reorder ani finish barcode scanner swift ui app

--- BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
@@ -7,47 +7,31 @@
 
 import SwiftUI
 
-struct AlertItem: Identifiable {
-    let id: UUID = UUID()
-    let title: String
-    let message: String
-    let dismissButton: Alert.Button
-}
-
-struct AlertContext {
-    static let invalidDeviceInput: AlertItem = AlertItem(
-        title: "Invalid Device Input",
-        message: "Something is wrong with the camera. We are unable to capture the input.",
-        dismissButton: .default(Text("OK")))
-    static let invalidScannedType: AlertItem = AlertItem(
-        title: "Invalid Scanned Type",
-        message: "The value scanned is not valid. This app scans EAN-8 and EAN-13.",
-        dismissButton: .default(Text("OK")))
-}
-
 struct BarcodeScannerView: View {
     
-    @State private var scannedCode: String = ""
-    @State private var alertItem: AlertItem?
+    @StateObject var viewModel = BarcodeScannerViewModel()
     
     var body: some View {
         NavigationView {
             VStack {
-                ScannerView(scannedCode: $scannedCode, alertItem: $alertItem)
+                ScannerView(
+                    scannedCode: $viewModel.scannedCode,
+                    alertItem: $viewModel.alertItem
+                )
                     .frame(maxWidth: .infinity, maxHeight: 300)
                 
                 Spacer().frame(height: 60)
                 
                 Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
                 
-                Text(scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode)
+                Text(viewModel.statusText)
                     .bold()
                     .font(.largeTitle)
-                    .foregroundColor(scannedCode.isEmpty ? .red : .green)
+                    .foregroundColor(viewModel.statusTextColor)
                     .padding()
             }
             .navigationTitle("Barcode Scanner")
-            .alert(item: $alertItem) { alertItem in
+            .alert(item: $viewModel.alertItem) { alertItem in
                 Alert(
                     title: Text(alertItem.title),
                     message: Text(alertItem.message),
